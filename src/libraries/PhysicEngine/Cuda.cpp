fc8203cf8fba8bcf49a0e6296e3ff386f9ae3324
traubenzucker3001 <traubenzucker3001@googlemail.com>
2015-01-05 20:48:17 +0100

    stepBYstepTOkernel-4.4
    - calcForces läuft doch (Haltepunkt war ungünstig festgelegt)
    *** GPU-Vers. läuft!!! ***

--- src/libraries/PhysicEngine/Cuda.cpp
+++ src/libraries/PhysicEngine/Cuda.cpp
@@ -376,7 +376,7 @@ void Cuda::stepCUDA(){
 	//schritte nacheinander aufrufen
 //	cout << "-test stepCUDA 1-" << endl; //zum debuggen
 //	int g = UniformGrid::getInstance()->getGridSize();
-//	resetGrid(d_gridCounters, d_gridCells, gridSize);		//versuch: g hier nicht erst in.cu
+	resetGrid(d_gridCounters, d_gridCells, gridSize);		//versuch: g hier nicht erst in.cu
 	
 	cudaMemcpy(h_gridCells, d_gridCells, bodyNum*sizeof(glm::ivec4), cudaMemcpyDeviceToHost);
 	for (int i = 0; i < 5; i++){
@@ -384,7 +384,7 @@ void Cuda::stepCUDA(){
 	}
 	
 //	cout << "-test stepCUDA 2-" << endl; //zum debuggen
-//	updateGrid(d_gridCounters, d_gridCells, d_pPos, d_pGridIndex);	//, d_voxelS, d_gridSL , d_gridMinPosVector
+	updateGrid(d_gridCounters, d_gridCells, d_pPos, d_pGridIndex);	//, d_voxelS, d_gridSL , d_gridMinPosVector
 
 	cudaMemcpy(h_gridCells, d_gridCells, bodyNum*sizeof(glm::ivec4), cudaMemcpyDeviceToHost);
 	for (int i = 0; i < 5; i++){
@@ -400,7 +400,7 @@ void Cuda::stepCUDA(){
 	}
 
 //	cout << "-test stepCUDA 4-" << endl; //zum debuggen
-//	updateMom(d_rbMass, d_rbForce, d_rbPos, d_rbLinMom, d_rbAngMom, d_pPos, d_pForce);	//, d_duration, d_termVeloc
+	updateMom(d_rbMass, d_rbForce, d_rbPos, d_rbLinMom, d_rbAngMom, d_pPos, d_pForce);	//, d_duration, d_termVeloc
 
 	cudaMemcpy(h_rbPos, d_rbPos, bodyNum*sizeof(glm::vec3), cudaMemcpyDeviceToHost);
 	for (int i = 0; i < 5; i++){
@@ -408,7 +408,7 @@ void Cuda::stepCUDA(){
 	}
 	
 //	cout << "-test stepCUDA 5-" << endl; //zum debuggen
-//	iterate(d_rbMass, d_rbPos, d_rbVeloc, d_rbLinMom, d_rbRotQuat, d_rbRotMat, d_rbAngVeloc, d_rbAngMom, d_rbInitInversInertTensDiago, d_rbInverseInertTens);	// d_duration, d_pRadius
+	iterate(d_rbMass, d_rbPos, d_rbVeloc, d_rbLinMom, d_rbRotQuat, d_rbRotMat, d_rbAngVeloc, d_rbAngMom, d_rbInitInversInertTensDiago, d_rbInverseInertTens);	// d_duration, d_pRadius
 	
 	cudaMemcpy(h_rbPos, d_rbPos, bodyNum*sizeof(glm::vec3), cudaMemcpyDeviceToHost);
 	for (int i = 0; i < 5; i++){
@@ -416,7 +416,7 @@ void Cuda::stepCUDA(){
 	}
 
 //	cout << "-test stepCUDA 6-" << endl; //zum debuggen
-//	updatePart(d_rbPos, d_rbVeloc, d_rbRotMat, d_rbAngVeloc, d_pPos, d_pVeloc);	//, d_pRadius
+	updatePart(d_rbPos, d_rbVeloc, d_rbRotMat, d_rbAngVeloc, d_pPos, d_pVeloc);	//, d_pRadius
 //	cout << "-test stepCUDA 7-" << endl; //zum debuggen	
 	//VOs updaten
 	//TODO
